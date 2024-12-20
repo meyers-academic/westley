7835a7c6f2b529c14e9dc688b3de1205e472bc69
Patrick Meyers <pmeyers@caltech.edu>
2024-12-20 14:11:24 -0800

    add new tests. change name of repo.

--- tests/test_fitter.py
+++ tests/test_fitter.py
@@ -70,6 +70,120 @@ class TestFitter:
         np.savetxt("height_samples.txt", results.heights[::100, 0])
         assert p_value > 0.05, f"Distribution of knot counts is not uniform (p={p_value:.3f})"
 
+    def test_model_initialization(self):
+        """Test model initialization and properties"""
+        assert self.bsm.N_possible_knots == 5
+        assert self.bsm.min_knots == 2
+        assert self.bsm.ylow == -2
+        assert self.bsm.yhigh == 2
+        assert self.bsm.yrange == 4
+        assert len(self.bsm.available_knots) == 5
+
+    def test_evaluate_interp_model_single_knot(self):
+        """Test interpolation with single knot"""
+        test_config = np.zeros(5, dtype=bool)
+        test_config[0] = True
+        test_heights = np.ones(5)
+        result = self.bsm.evaluate_interp_model(np.array([2.5]), test_heights, test_config, self.bsm.available_knots)
+        assert np.allclose(result, 1.0)
+
+    def test_evaluate_interp_model_multiple_knots(self):
+        """Test interpolation with multiple knots"""
+        test_config = np.ones(5, dtype=bool)
+        test_heights = np.ones(5)
+        x_test = np.linspace(0, 5, 10)
+        result = self.bsm.evaluate_interp_model(x_test, test_heights, test_config, self.bsm.available_knots)
+        assert len(result) == len(x_test)
+        assert np.allclose(result, 1.0)
+
+    def test_birth_proposal_mechanics(self):
+        """Test birth proposal basics"""
+        self.bsm.configuration = np.zeros(5, dtype=bool)
+        self.bsm.configuration[0:2] = True  # Start with minimum knots
+        
+        ll, logR, config, heights, knots = self.bsm.birth()
+        
+        if ll != -np.inf:  # If proposal was valid
+            assert np.sum(config) == 3  # Should have one more knot
+            assert heights.shape == (5,)
+            assert knots.shape == (5,)
+
+    def test_death_proposal_mechanics(self):
+        """Test death proposal basics"""
+        self.bsm.configuration = np.ones(5, dtype=bool)
+        
+        ll, logR, config, heights, knots = self.bsm.death()
+        
+        if ll != -np.inf:  # If proposal was valid
+            assert np.sum(config) == 4  # Should have one less knot
+            assert heights.shape == (5,)
+            assert knots.shape == (5,)
+
+    def test_change_amplitude_gaussian_mechanics(self):
+        """Test Gaussian amplitude change proposal"""
+        original_heights = self.bsm.current_heights.copy()
+        
+        ll, logR, config, heights, knots = self.bsm.change_amplitude_gaussian()
+        
+        if ll != -np.inf:
+            # Only one height should change
+            diff = heights != original_heights
+            assert np.sum(diff) == 1
+            # Changed height should be within bounds
+            assert np.all(heights >= self.bsm.ylow)
+            assert np.all(heights <= self.bsm.yhigh)
+
+    def test_change_amplitude_prior_draw_mechanics(self):
+        """Test prior draw amplitude change proposal"""
+        original_heights = self.bsm.current_heights.copy()
+        
+        ll, logR, config, heights, knots = self.bsm.change_amplitude_prior_draw()
+        
+        if ll != -np.inf:
+            # Only one height should change
+            diff = heights != original_heights
+            assert np.sum(diff) == 1
+            # New height should be within prior bounds
+            assert np.all(heights >= self.bsm.ylow)
+            assert np.all(heights <= self.bsm.yhigh)
+
+    def test_change_knot_location_mechanics(self):
+        """Test knot location change proposal"""
+        original_knots = self.bsm.available_knots.copy()
+        
+        ll, logR, config, heights, knots = self.bsm.change_knot_location()
+        
+        if ll != -np.inf:
+            # Only one knot should change
+            diff = knots != original_knots
+            assert np.sum(diff) == 1
+            # New knot should be within bounds
+            idx_changed = np.where(diff)[0][0]
+            assert knots[idx_changed] >= self.bsm.xlows[idx_changed]
+            assert knots[idx_changed] <= self.bsm.xhighs[idx_changed]
+
+    def test_height_prior_bounds(self):
+        """Test height prior boundary conditions"""
+        # Test below lower bound
+        assert self.bsm.get_height_log_prior(self.bsm.ylow - 1) == -np.inf
+        # Test above upper bound
+        assert self.bsm.get_height_log_prior(self.bsm.yhigh + 1) == -np.inf
+        # Test valid height
+        valid_height = (self.bsm.yhigh + self.bsm.ylow) / 2
+        assert self.bsm.get_height_log_prior(valid_height) == -np.log(self.bsm.yrange)
+
+    def test_amplitude_prior_draw_proposal(self):
+        """Test that amplitude prior draw actually changes values"""
+        initial_height = self.bsm.current_heights[0]
+        
+        # Perform several proposals
+        different_values = set()
+        for _ in range(100):
+            new_ll, ratio, new_config, new_heights, new_knots = self.bsm.change_amplitude_prior_draw()
+            different_values.add(new_heights[0])
+        
+        # Should get multiple different values
+        assert len(different_values) > 1, "Amplitude proposals not generating different values"
 
 def check_uniform_distribution(arr, bins=10, discrete=False, range=None):
     # for the discrete case, we need to use the unique values
@@ -87,4 +201,3 @@ def check_uniform_distribution(arr, bins=10, discrete=False, range=None):
         ks_stat, p_value = kstest(arr, 'uniform', args=(range[0], range[1]))
 
     return p_value
-
